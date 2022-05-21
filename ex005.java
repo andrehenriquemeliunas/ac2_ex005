61be1c6bed594df9482d6193a4bb50a8513ff057
Andre Henrique <andremeliunas@hotmail.com>
2022-05-21 20:26:13 -0300

    Update ex005.java

--- ex005.java
+++ ex005.java
@@ -1,53 +1,48 @@
 import java.util.Scanner;
 public class ex005 {
     public static void main( String[] args){
-        int cod;
-        double n1, n2, n3;
-        double media;
+        int cod; //Declaração das variáveis
+        double n1, n2, n3;//Declaração das variáveis
+        double media;//Declaração das variáveis
+        String sit;//Declaração das variáveis
         
-        Scanner s = new Scanner( System.in);
+        Scanner s = new Scanner( System.in);//Inicia o scanner
         
-        do{
-            System.out.print("Digite o código do aluno (0 - Encerra o programa): ");
-            cod = s.nextInt();
+        do{//Laço de repetição
+            System.out.print("Digite o código do aluno (0 - Encerra o programa): ");//Pergunta o código do aluno
+            cod = s.nextInt();//Armazena o código do aluno
 
-            if (cod == 0){
-                System.out.print("Obrigado por usar o programa! FIM!");
-            }else{
-                System.out.print("Digite a nota 1: ");
-                n1 = s.nextDouble();
+            if (cod == 0){//Verifica se o usuário digitou 0 para falar que o programa encerrou
+                System.out.print("Obrigado por usar o programa! FIM!");//Mostra ao usuário que o prog. acabou
+            }else{//Se o usuário não digitou 0, continua
+                System.out.print("Digite a nota 1: ");//Pede a nota 1
+                n1 = s.nextDouble();//Armazena nota 1
 
-                System.out.print("Digite a nota 2: ");
-                n2 = s.nextDouble();
+                System.out.print("Digite a nota 2: ");//Pede a nota 2
+                n2 = s.nextDouble();//Armazena nota 2
 
-                System.out.print("Digite a nota 3: ");
-                n3 = s.nextDouble();
+                System.out.print("Digite a nota 3: ");//Pede a nota 3
+                n3 = s.nextDouble();//Armazena nota 3
 
-                media = (n1 + n2 + n3) / 3;
+                media = (n1 + n2 + n3) / 3;//Calculo da média
 
-                if(media >= 8){
-                    System.out.println("-------------------------");
-                    System.out.println("Código aluno: "+ cod);
-                    System.out.println("Média: "+ media);
-                    System.out.println("Situação: APROVADO.");
-                    System.out.println("-------------------------");
-                }else if(media > 6){
-                    System.out.println("-------------------------");
-                    System.out.println("Aluno: "+ cod);
-                    System.out.println("Média: "+ media);
-                    System.out.println("Situação: RECUPERAÇÃO");
-                    System.out.println("-------------------------");
-                }else{
-                    System.out.println("-------------------------");
-                    System.out.println("Aluno: "+ cod);
-                    System.out.println("Média: "+ media);
-                    System.out.println("Situação: REPROVADO");
-                    System.out.println("-------------------------");
+                if(media >= 8){//Verificação da nota do aluno, se foi mais que 8
+                    sit = "APROVADO";//Colaca na variável sit a situação do aluno
+                }else if(media > 6){//Verificação da nota do aluno, se foi mais que 6
+                    sit = "RECUPERAÇÃO";//Colaca na variável sit a situação do aluno
+                }else{//Verificação da nota do aluno, se foi menos que 6
+                    sit = "REPROVADO";//Colaca na variável sit a situação do aluno
                 }
+
+                System.out.println("-------------------------");//Apenas questão de estética
+                System.out.println("Aluno: "+ cod);//Mostra o código do aluno
+                System.out.println("Média: "+ media);//Mostra a média
+                System.out.println("Situação: " + sit);//Mostra a situação
+                System.out.println("-------------------------");//Apenas questão de estética
             }
             
-        }while(cod != 0);
+        }while(cod != 0);//Encerra o laçõ de repetição
 
-        s.close();
+        s.close();//Fecha o scanner
     }
 }
